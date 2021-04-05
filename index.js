2989cf452ee4d00d9cba045180da8ad23a248049
IvanShyvanau <kippbelarus@gmail.com>
2021-04-06 02:17:00 +0300

    concat, indexOf, includes

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
+//Выполнение первого задания 
 let text = "Лето удалось очень жарким, было много ягод и фруктов, лучшим местом для отдыха была речка"; //создал строку
 console.log(text);
 
-
 let changed = text.split(',');  //сделал массив
 for ( let i of changed ){
-  i = i.charAt(0).toUpperCase() + i.substr(1);
+  i = i.charAt(0).toUpperCase() + i.substr(1); //Заглавие
 }
 //console.log(changed);
 
@@ -14,12 +13,14 @@ let finish = changed.join('.');  //вернул строку
 //console.log(finish);
 
 var str = finish;
-var newstr = str.replace(/было/i, 'Было');
+var newstr = str.replace(/было/i, 'Было'); //сделал слово с большой буквы
 var strSecond = newstr;
-var newstrSecond = strSecond.replace(/лучшим/i, 'Лучшим');
+var newstrSecond = strSecond.replace(/лучшим/i, 'Лучшим'); //сделал слово с большой буквы
 console.log(newstrSecond);
 
 /*
+//тчетные попытки на которые можно не обращать внимания
+
 let newText = finish.split('');
 console.log(newText);
 
@@ -34,22 +35,24 @@ let finish = array.prototype.map.call(text, function(v){
 console.log(changedText);
 console.log(finish);*/
 
+// выполнение второго задания 
 
 let secondEx = "Антон, Дима и Женя пришли на День рождения сразу, а Миша пришел позже";
 console.log(secondEx);
-let secondExChanged = secondEx.split(' ');
+let secondExChanged = secondEx.split(' '); //привел строку к массиву
 //console.log(secondExChanged);
-let three = secondExChanged.splice(0, 6, 'На');
+let three = secondExChanged.splice(0, 6, 'На');  //перебор массива 
 //console.log(secondExChanged);
-let four = secondExChanged.splice(3, 1, 'пришли:');
+let four = secondExChanged.splice(3, 1, 'пришли:');  //перебор массива 
 //console.log(secondExChanged);
-let five = secondExChanged.splice(4, 1, 'Антон, Дима, Женя,');
+let five = secondExChanged.splice(4, 1, 'Антон, Дима, Женя,');  //перебор массива 
 //console.log(secondExChanged);
-let six = secondExChanged.splice(6, 2);
+let six = secondExChanged.splice(6, 2);  //перебор массива 
 //console.log(secondExChanged);
-let seven = secondExChanged.join(' ');
+let seven = secondExChanged.join(' ');  //привел к строке
 console.log(seven);
 
+// Выполнение третьего задания
 
 for (i = 1; i <= 100; i++){
   if(i%3 === 0 && i%5 === 0){
@@ -62,3 +65,25 @@ for (i = 1; i <= 100; i++){
     console.log(i);
   }
 }
+ // метод includes
+let mass = [1, "pet", "ivan"];
+console.log(mass.includes(1));
+let massTwo = ["FirstName", "LastName"]
+if (massTwo.includes ("FirstName")){
+    console.log("you are right");
+}else{
+    console.log("sorry");
+}
+
+// метод indexOf
+let arrayTwo = ["ivan", "Lena", "sergei"];
+console.log (arrayTwo.indexOf("Lena"));
+console.log(arrayTwo.indexOf("ivan"));
+
+//метод concat
+let firstArray = ['first name', 'last name',];
+let secondArray = ['age', 'gender'];
+let thirthArray = firstArray.concat(secondArray);
+console.log(thirthArray);
+
+
